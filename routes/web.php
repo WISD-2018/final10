b1d1cf3e92cc9d3ab61aad32b77627f16ae1cf03
3A532067 <s3a532067@student.ncut.edu.tw>
2019-01-05 23:29:30 +0800

    新增內建的登入註冊功能

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('post', function () {
+    return view('post');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
