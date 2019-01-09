cb243063b464bbcb7c598d3d8e25e07e873c3107
3A532067 <s3a532067@student.ncut.edu.tw>
2019-01-09 15:16:23 +0800

    建立所有controller

---
+++ app/Http/Controllers/PraiseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Praise;
+use Illuminate\Http\Request;
+
+class PraiseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Praise  $praise
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Praise $praise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Praise  $praise
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Praise $praise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Praise  $praise
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Praise $praise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Praise  $praise
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Praise $praise)
+    {
+        //
+    }
+}
