90eb0f10fd9ba265690be814c27a6ab37e82d078
3A532067 <s3a532067@student.ncut.edu.tw>
2019-01-09 15:17:08 +0800

    建立所有migration

---
+++ database/migrations/2019_01_09_055456_create_praises_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePraisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('praises', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('art_id');
+            $table->unsignedInteger('stu_id');
+            $table->boolean('praise');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('praises');
+    }
+}
