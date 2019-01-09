3341094cae6409b907155e18e01bae9fba6c4f03
3A532067 <s3a532067@student.ncut.edu.tw>
2019-01-09 18:38:41 +0800

    修改users table 的屬性

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('permission');
-            $table->string('photo');
+            $table->integer('permission')->default(0);
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
