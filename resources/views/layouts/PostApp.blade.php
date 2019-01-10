6a91f97a217614b43f29b9f608c8b46b1ddb5860
3A532067 <s3a532067@student.ncut.edu.tw>
2019-01-10 19:11:36 +0800

    插入icon link 、新增圓形圖片css、在導覽欄新增使用者圖片和姓名

--- resources/views/layouts/PostApp.blade.php
+++ resources/views/layouts/PostApp.blade.php
@@ -19,15 +19,30 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+
     <style>
         html body {
             background-color: #00324e;
         }
+        .pic {
+            position:relative;
+            width: 40px;
+            height: 40px;
+            overflow: hidden;
+            border-radius:50%;
+        }
+        .pic img {
+            width: 100%;
+            height: auto;
+        }
+        .navbar-right{float:right!important;margin-right:-200px}
+
     </style>
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="background-color:#006aa6;" >
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel " style="background-color:#006aa6;" >
         <div class="container">
 
             <a class="navbar-brand text-white" href="{{ url('/') }}">
@@ -44,6 +59,32 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
+            @if(Auth::check())
+                <a href="">
+                    <div class="navbar-right">
+                        <div class="pic">
+                            <img src="{{ Auth::user()->photo }}">
+                        </div>
+                    </div>
+                </a>
+                <span class="text-white">{{ Auth::user()->name }}</span>
+
+            @else
+                <div>
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-circle fa-stack-2x"></i>
+                        <i class="fa fa-user fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <sapn class="font-weight-bold text-white ">訪客身份</sapn>
+                </div>
+            @endif
+
+
+
+
+
+
 
 
 
