dce1c7f8ab01673298a73a1fab021ac867d694e4
3A532055 <ylhc214044@gmail.com>
2019-01-15 13:52:43 +0800

    建立所有User的方法

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Article as ArticleEloquent;
 use App\User as UserEloquent;
 use App\Praise as PraiseEloquent;
+use App\User;
 
 class UserController extends Controller
 {
@@ -24,7 +25,9 @@ class UserController extends Controller
     }
     public function back()
     {
-        return view('back.UserBack');
+        $users =User::orderBy('created_at','DESC')->get();
+        $data=['users'=>$users];
+        return view('back.UserBack',$data);
     }
 
     public function index()
@@ -36,6 +39,7 @@ class UserController extends Controller
         $data=['articles'=>$articles,'posts'=>$posts,'users'=>$users,'praises'=>$praises];
         return view('user',$data);
     }
+
 
     /**
      * Show the form for creating a new resource.
