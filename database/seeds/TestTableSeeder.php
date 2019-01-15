d51ada3e0d7996ce1bd8110cf5de3a6e81177207
3A532067 <s3a532067@student.ncut.edu.tw>
2019-01-16 07:35:03 +0800

    假資料更新

--- database/seeds/TestTableSeeder.php
+++ database/seeds/TestTableSeeder.php
@@ -18,30 +18,30 @@ class TestTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(UserEloquent::class, 20)->create();
+        $users = factory(UserEloquent::class, 30)->create();
         $restaurants = factory(RestaurantEloquent::class, 20)->create();
-        $articles = factory(ArticleEloquent::class, 20)->create()->each(function ($article) use ($restaurants, $users) {
-            $article->res_id = $restaurants[mt_rand(0, (count($restaurants) - 1))]->id;
-            $article->stu_id = $users[mt_rand(1, (count($users) - 1))]->id;
+        $articles = factory(ArticleEloquent::class, 300)->create()->each(function ($article) use ($restaurants, $users) {
+            $article->res_id = $restaurants[mt_rand(1, (count($restaurants) - 1 ))]->id;
+            $article->stu_id = $users[mt_rand(2, (count($users) - 1))]->id;
             $article->save();
         });
-        $messages = factory(MessageEloquent::class, 20)->create()->each(function ($message) use ($articles, $users) {
-            $message->art_id = $articles[mt_rand(0, (count($articles) - 1))]->id;
-            $message->stu_id = $users[mt_rand(1, (count($users) - 1))]->id;
+        $messages = factory(MessageEloquent::class, 300)->create()->each(function ($message) use ($articles, $users) {
+            $message->art_id = $articles[mt_rand(1, (count($articles) - 1))]->id;
+            $message->stu_id = $users[mt_rand(2, (count($users) - 1))]->id;
             $message->save();
         });
-        $praises = factory(PraiseEloquent::class, 20)->create()->each(function ($praise) use ($articles, $users) {
-            $praise->art_id = $articles[mt_rand(0, (count($articles) - 1))]->id;
-            $praise->stu_id = $users[mt_rand(1, (count($users) - 1))]->id;
+        $praises = factory(PraiseEloquent::class, 300)->create()->each(function ($praise) use ($articles, $users) {
+            $praise->art_id = $articles[mt_rand(1, (count($articles) - 1))]->id;
+            $praise->stu_id = $users[mt_rand(2, (count($users) - 1))]->id;
             $praise->save();
         });
         $problems = factory(ProblemEloquent::class, 20)->create()->each(function ($problem) use ($users) {
-            $problem->stu_id = $users[mt_rand(1, (count($users) - 1))]->id;
+            $problem->stu_id = $users[mt_rand(2, (count($users) - 1))]->id;
             $problem->save();
         });
-        $scores = factory(ScoreEloquent::class, 20)->create()->each(function ($score) use ($restaurants, $users) {
+        $scores = factory(ScoreEloquent::class, 100)->create()->each(function ($score) use ($restaurants, $users) {
             $score->res_id = $restaurants[mt_rand(0, (count($restaurants) - 1))]->id;
-            $score->stu_id = $users[mt_rand(1, (count($users) - 1))]->id;
+            $score->stu_id = $users[mt_rand(2, (count($users) - 1))]->id;
             $score->save();
         });
     }
